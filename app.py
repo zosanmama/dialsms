127c088161367872952f57d0a85ab77daa83e974
Aiko <junemomohanamaru@gmail.com>
2025-03-31 16:31:18 +0900

    secret key added

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 from flask import Flask, request, render_template, session
 
 app = Flask(__name__)
-app.secret_key = 'your_secret_key'  # セッション用の秘密鍵（適当に変えてOK）
+app.secret_key = os.environ.get('SECRET_KEY', 'your_secret_key')
 
 @app.route('/webhook', methods=['POST'])
 def webhook():
